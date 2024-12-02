b817e8c644f3c67bf20cbf1eb3c60456a3dc4b16
AnnieMarie <anniemariepetersen@gmail.com>
2024-12-02 21:19:47 +0530

    Update urls.py

--- api/User/urls.py
+++ api/User/urls.py
@@ -1,1 +1,14 @@
 
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+from bookapi.user.views import registration_view, logout_view
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('account/token', obtain_auth_token),
+    path('account/registration', registration_view),
+    path('account/logout/', logout_view),
+    path('account/jtoken/', TokenObtainPairView.as_view()),
+    path('account/Resfresh/', TokenRefreshView.as_view()),
+   
+]
