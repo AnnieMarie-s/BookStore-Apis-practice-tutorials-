0e4857879051c5ce603a2fd2bc970ca95a5dc7ee
AnnieMarie <anniemariepetersen@gmail.com>
2024-12-02 21:15:11 +0530

    Update Serializers.py

--- api/Serializers.py
+++ api/Serializers.py
@@ -1,1 +1,16 @@
+from rest_framework import serializers
+from bookapi.models import bookmodel, Reviewmodel
 
+class boolserializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only = True)
+    class Meta:
+        model = bookmodel
+        fields = '__all__'
+        # extra_kwargs = {'user':{'write_only':True}}
+        
+        
+    # def validate(self, data):
+    #     bookdata = bookmodel.objects.filter(title =data['title']).exists()
+    #     if(bookdata):
+    #        raise serializers.ValidationError({'title':'this book is already added'})
+    #     return data
