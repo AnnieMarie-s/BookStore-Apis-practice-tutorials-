b16821e8c53ea5816b9dc9a4e3872505f781a563
AnnieMarie <anniemariepetersen@gmail.com>
2024-12-02 21:17:01 +0530

    Update pagenation.py

--- api/pagenation.py
+++ api/pagenation.py
@@ -1,1 +1,14 @@
+from rest_framework.pagination import PageNumberPagination, CursorPagination, LimitOffsetPagination
 
+class  custompagepagination(PageNumberPagination):
+     page_size = 1
+     page_query_param ='p'
+     
+
+class  customcursoepagination(CursorPagination):
+     page_size = 1
+     page_query_param ='p'
+     
+
+class  customlimitpagination(LimitOffsetPagination):
+     default_limit = 1
