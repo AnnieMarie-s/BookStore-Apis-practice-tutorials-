add87c8f0fd2d2867af2321b4e37e1ba01261edc
AnnieMarie <anniemariepetersen@gmail.com>
2024-12-02 21:14:31 +0530

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,1 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
 
+# Create your models here.
+
+
+
+class bookmodel(models.Model):
+    title = models.CharField(max_length=100)
+    langauge = models.CharField(max_length=100)
+    edition = models.CharField(max_length=100)
+    genere = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
